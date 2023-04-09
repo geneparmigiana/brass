e8fc1d443b892d745ef39ea6842e79e9574fd9d1
Gene Parmigiana <thaversang1@gmail.com>
2023-04-09 00:30:53 -0400

    fixing routers

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -6,6 +6,6 @@ from app.apps.user.router.public_api import user_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router, tags=["login"])
-api_router.include_router(user_router.router, prefix="/users", tags=["users"])
+api_router.include_router(user_router, prefix="/users", tags=["users"])
 # api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
