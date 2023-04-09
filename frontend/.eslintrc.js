d1f0bcfdcf84d6f96b289d72c74d8a2997b46e5d
Gene Parmigiana <thaversang1@gmail.com>
2023-04-09 10:47:40 -0400

    using svelte vite

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,51 @@
+node_modules
+const OFF = 0,
+  ERROR = 2;
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: [
+    "!.eslintrc.js",
+    "!.prettierrc.js",
+    "node_modules/",
+    "shims-tsx.d.ts",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console":
+      process.env.NODE_ENV === "production" ? [ERROR, { allow: ["error"] }] : OFF,
+    "no-debugger": process.env.NODE_ENV === "production" ? ERROR : OFF,
+    "@typescript-eslint/explicit-module-boundary-types": OFF,
+    "@typescript-eslint/no-unused-vars": [
+      ERROR,
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ["**/registerServiceWorker.ts"],
+      rules: {
+        "no-console": OFF,
+      },
+    },
+  ],
+};
