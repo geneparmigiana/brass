19e4851f257b4d845c8192ad8b50e734a9833ecb
Gene Parmigiana <thaversang1@gmail.com>
2023-04-09 00:37:45 -0400

    straggler

--- backend/app/app/apps/user/router/public_api.py
+++ backend/app/app/apps/user/router/public_api.py
@@ -46,7 +46,7 @@ async def create_user(
             status_code=400,
             detail="The user with this username already exists in the system.",
         )
-    user = await crud.user.create(db, obj_in=user_in)
+    user = await UserDAO.create(db, obj_in=user_in)
     # if settings.EMAILS_ENABLED and user_in.email:
     #     send_new_account_email(
     #         email_to=user_in.email, username=user_in.email, password=user_in.password
