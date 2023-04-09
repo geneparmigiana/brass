4366b488157014fbb0b282043dff425b41db9b53
Gene Parmigiana <thaversang1@gmail.com>
2023-04-09 00:23:43 -0400

    last bug

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.apps.auth.router.public_api import router as auth_router 
 from app.apps.user.router.public_api import router as user_router 
 
-from app.api.api_v1.endpoints import items, login, users, utils
+#from app.api.api_v1.endpoints import items, login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(auth_router.router, tags=["login"])
